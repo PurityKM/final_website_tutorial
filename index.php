58e15a309fa108e7634f826da8246e09b4cca384
PurityKM <irungu1210@gmail.com>
2023-06-02 16:21:09 +0300

    This the the home and the main  page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    define("TITLE", "Home| Practice Website");
+
+    include('includes/header.php');
+?>
+
+<div class="philosophy">
+    <hr>
+    <h1>Purity's Philosophy of Fine Dining</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.</p>
+    <p>You can call Purity's teams here.</p>
+    <hr>
+</div>
+
+
+
+
+<?php
+    include('includes/footer.php');
+?>

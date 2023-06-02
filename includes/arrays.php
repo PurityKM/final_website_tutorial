9202e76b8173d2c36e6f260c069f444b12a3791c
PurityKM <irungu1210@gmail.com>
2023-06-02 16:20:18 +0300

    The includes i have included in my pages in order to avoid repeating my codes

---
+++ includes/arrays.php
@@ -0,0 +1,77 @@
+<?php
+
+    //Nav Menu Item
+
+    $navItems = array(
+        array(
+            'slug'     => 'index.php',
+            'title'    => 'Home'
+        ),
+        array(
+            'slug'     => 'team.php',
+            'title'    => 'Team'
+        ),
+        array(
+            'slug'     => 'menu.php',
+            'title'    => 'Menu'
+        ),
+        array(
+            'slug'     => 'contact.php',
+            'title'    => 'Contact'
+        ),
+    );
+
+    //Team Members
+
+    $teamMembers = array(
+        array(
+            'name'        => 'Purity',
+            'position'    => 'Owner',
+            'bio'         => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+            'img'         => 'w-1'
+        ),
+        array(
+            'name'        => 'Maggie',
+            'position'    => 'Supervisor',
+            'bio'         => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+            'img'         => 'w-2'
+        ),
+        array(
+            'name'        => 'Kamaa',
+            'position'    => 'General Manager',
+            'bio'         => 'Lorem ipsum dolor sit    amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+            'img'         => 'w-4'
+        )  
+    );
+
+    //Menu Items
+
+    $menuItems = array(
+        "club-sandwich"     => array(
+                'title'     => 'Club Sandwich',
+                'price'     => 11,
+                'blurb'     => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+                'drink'     => 'Club Soda'
+        ),
+
+        "dill-salmon"       => array(
+                'title'     => 'Lemon &amp; Dill Salmon',
+                'price'     => 17,
+                'blurb'     => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+                'drink'     => 'Fancy Wine'
+        ),
+        "super-salad"       => array(
+                'title'     => 'The Super Salad<sup>&reg;</sup>',
+                'price'     => 25,
+                'blurb'     => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+                'drink'     => 'Jug " Water'
+        ),
+        "mexican-barbacoa"  => array(
+                'title'     => 'Mexican Barbacoa',
+                'price'     => 30,
+                'blurb'     => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit tempore dolorum sapiente optio excepturi quis culpa repellendus. Repellat, perferendis officiis.',
+                'drink'     => 'Beer with a lime'
+        ),
+    );
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<?php
+    $companyName = "PurityKM";
+    include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo TITLE; ?></title>
+        <link rel="stylesheet" href="final_website_style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="banner">
+                <a href="#" title="Return to Home">
+                    <img src="img/advi2.jpg" alt="">
+                </a>
+            </div>
+            <div class="nav"> <!--banner-end-->
+                    <?php include('includes/nav.php'); ?>
+            </div>
+            <div class="content">
